99cf6c7129fd8929af6f22925ea67e97636e5acd
Gerald Acero <senpaiacero@gmail.com>
2024-03-02 00:48:48 +0800

    Worked on ignoring SSL certificate errors

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
 
 def ActivateKP():
-    pass
+    edge_driver_path = ""
+
+    edge_options = webdriver.EdgeOptions()
+    edge_options.add_argument("ignore-certificate-errors")
 
 def DeactivateKP():
     pass
