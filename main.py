d08b1a949748b4ec38ee3e05142df1e80e1328e4
Gerald Acero <senpaiacero@gmail.com>
2024-03-02 01:01:29 +0800

    Worked on new instance of the Edge driver with options and desired capabilities

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from selenium import webdriver
 
+edge_driver_path = ""
+local_network_url = ""
+username = ""
+password = ""
 def ActivateKP():
-    edge_driver_path = ""
-
+    
     edge_options = webdriver.EdgeOptions()
     edge_options.add_argument("ignore-certificate-errors")
+
+    capabilities = {
+        'acceptInsecureCerts': True
+    }
+
+    driver = webdriver.Edge(executable_path=edge_driver_path, options=edge_options, capabilities=capabilities)
+
+    driver.get(local_network_url)
 
 def DeactivateKP():
     pass
