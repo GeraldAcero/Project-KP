717739f5fa5aee9bd674ed8db9ee6ab92e3b2bc7
Gerald Acero <senpaiacero@gmail.com>
2024-03-02 01:24:21 +0800

    Worked on username and password element automation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 
+
 edge_driver_path = ""
 local_network_url = ""
 username = ""
@@ -16,6 +17,15 @@ def ActivateKP():
     driver = webdriver.Edge(executable_path=edge_driver_path, options=edge_options, capabilities=capabilities)
 
     driver.get(local_network_url)
+
+    username_input = driver.find_element_by_id("user_name")
+    username_input.send_keys(username)
+
+    password_input = driver.find_element_by_id("loginpp")
+    password_input.send_keys(password)
+
+    login_button = driver.find_element_by_id("login_btn")
+    login_button.click()
 
 def DeactivateKP():
     pass
