93f0a89a38cbcc1e695757e05f79de25ff40da29
Gerald Acero <senpaiacero@gmail.com>
2024-03-04 10:27:18 +0800

    Worked on DeactiveKP

--- main.py
+++ main.py
@@ -52,9 +52,51 @@ def ActivateKP(username, password, edge_path):
     apply_button.click()
     driver.quit()
 
-def DeactivateKP():
-    pass
+def DeactivateKP(username, password, edge_path):
+
+    edge_options = webdriver.EdgeOptions()
+    edge_options.add_argument("ignore-certificate-errors")
+
+    capabilities = {
+        'acceptInsecureCerts': True
+    }
+
+    driver = webdriver.Edge(executable_path=edge_path, options=edge_options, capabilities=capabilities)
+
+    driver.get("https://192.168.1.1/html/login_pldt.html")
+
+    username_input = driver.find_element_by_id("user_name")
+    username_input.send_keys(username)
+
+
+    password_input = driver.find_element_by_id("loginpp")
+    password_input.send_keys(password)
+    time.sleep(1)
+
+    login_button = driver.find_element_by_id("login_btn")
+    login_button.click()
+    time.sleep(5)
+
+    driver.get("https://192.168.1.1/html/firewall_enable_inter.html")
+
+    radio_button = driver.find_element_by_id("firewall_low")
+    radio_button.click()
+    time.sleep(1)
+
+    apply_button = driver.find_element_by_xpath("//input[@type='button' and @value='Apply']")
+    apply_button.click()
+
+    time.sleep(1)
+    driver.get("https://192.168.1.1/html/parental_control_inter.html")
+
+
+    radio_button = driver.find_element_by_xpath("//input[@type='radio' and @name='parent_control_enable' and @value='0']")
+    radio_button.click()
+    time.sleep(1)
+    apply_button = driver.find_element_by_xpath("//input[@type='button' and @value='Apply']")
+    apply_button.click()
+    driver.quit()
 
 
 config = load_config()
-ActivateKP(config['username'], config['password'], config['edge_path'])
+DeactivateKP(config['username'], config['password'], config['edge_path'])
