a28328d45d5e0dbdc930d467b90b822e0788e6f7
Gerald Acero <senpaiacero@gmail.com>
2024-03-04 09:51:19 +0800

    Import time to add delay and remove unused code

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
+import time
 
+edge_driver_path = "EDGE PATH"
 
-edge_driver_path = ""
-local_network_url = ""
-username = ""
-password = ""
+username = "USER"
+password = "PASS"
+
 def ActivateKP():
     
     edge_options = webdriver.EdgeOptions()
@@ -16,7 +17,7 @@ def ActivateKP():
 
     driver = webdriver.Edge(executable_path=edge_driver_path, options=edge_options, capabilities=capabilities)
 
-    driver.get(local_network_url)
+    driver.get("https://192.168.1.1/html/login_pldt.html")
 
     username_input = driver.find_element_by_id("user_name")
     username_input.send_keys(username)
